bad5a22d7d9ff2f0b6354322c6c12df6c87655ed
johnnyfsa <joaofeirense@gmail.com>
2024-06-04 16:01:06 -0300

    first commit

---
+++ src/main/java/br/com/biscoithor/randomPhrases/RandomPhrasesApplication.java
@@ -0,0 +1,13 @@
+package br.com.biscoithor.randomPhrases;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandomPhrasesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RandomPhrasesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/biscoithor/randomPhrases/RandomPhrasesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.biscoithor.randomPhrases;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RandomPhrasesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
